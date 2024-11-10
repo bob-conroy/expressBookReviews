be61b93e18d19765ae2ac1d370d729dcf42afd43
bob-conroy <bob.conroymba@gmail.com>
2024-11-10 12:47:02 -0500

    Updates made

--- final_project/router/generalOrig.js
+++ final_project/router/generalOrig.js
@@ -5,7 +5,7 @@ let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
 public_users.post("/register", (req,res) => {
-    const username = req.body.username;
+  const username = req.body.username;
     const password = req.body.password;
 
     // Check if both username and password are provided
@@ -25,82 +25,55 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-    let getBooks = new Promise((resolve,reject) => {
-        resolve(JSON.stringify(books,null,4));
-    })
-
-    getBooks.then( ( successMessage) => {
-        res.send(successMessage);
-    })
+  res.send(JSON.stringify(books,null,4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
     const isbn = req.params.isbn;
-    let getBooks = new Promise((resolve,reject) => {
-        resolve(books[isbn]);
-    })
-
-    getBooks.then( ( successMessage) => {
-        res.send(successMessage);
-    })
+    res.send(books[isbn]);
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-    const author = req.params.author;
-    let authBooks = []
-    let getBooks = new Promise((resolve,reject) => {
-
-        if (author) {
-            for(var i in books){
-                if (books[i].author === author) {
-                    authBooks.push(books[i]);
-                }
-            }    
-            if (authBooks.length > 0) {
-             resolve(JSON.stringify(authBooks,null,4));
-            } else {
-             reject("Author not found");
-            }
+  const author = req.params.author;
+  let authBooks = []
+  if (author) {
+    for(var i in books){
+        if (books[i].author === author) {
+            authBooks.push(books[i]);
         }
-    });
+    }    
+    if (authBooks.length > 0) {
+       res.send(JSON.stringify(authBooks,null,4));
+    } else {
+        return res.status(300).json({message: "Author not found"});
+    }
 
-    getBooks.then( ( successMessage) => {
-        res.send(successMessage);
-    })
-    .catch((err) => {
-        return res.status(300).json({message: err});
-        });
-  
+  } else {
+    return res.status(300).json({message: "Author not provided"});
+  }
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
     const title = req.params.title;
     let titleBooks = []
-    
-    let getBooks = new Promise((resolve,reject) => {
-        if (title) {
-            for(var i in books){
-                if (books[i].title === title) {
-                    titleBooks.push(books[i]);
-                }
-            }    
-            if (titleBooks.length > 0) {
-                resolve(JSON.stringify(titleBooks,null,4));
-            } else {
-                reject ("Title not found");
-            }
-        }
-    });
-
-    getBooks.then( ( successMessage) => {
-        res.send(successMessage);
-    })
-    .catch((err) => {
-        return res.status(300).json({message: err});
-    });
+    if (title) {
+      for(var i in books){
+          if (books[i].title === title) {
+            titleBooks.push(books[i]);
+          }
+      }    
+      if (titleBooks.length > 0) {
+         res.send(JSON.stringify(titleBooks,null,4));
+      } else {
+          return res.status(300).json({message: "Title not found"});
+      }
+  
+    } else {
+      return res.status(300).json({message: "Title not provided"});
+    }
 });
 
 //  Get book review
