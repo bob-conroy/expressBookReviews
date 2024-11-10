4708a3b460dd687a96850a663e9c625cdf47f50f
bob-conroy <bob.conroymba@gmail.com>
2024-11-10 11:30:58 -0500

    Updates made

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -64,8 +64,15 @@ regd_users.put("/auth/review/:isbn/:review", (req, res) => {
     const review = req.params.review;
     const username = req.session.authorization['username'];
     const book = books[isbn];
+    let origReview = books[isbn].reviews;
+    console.log(origReview);
+    if (origReview[username]) {
+            origReview[username] = review;
+            book.reviews = origReview;
+            console.log(book.reviews);
+    } else {
 
-    book.reviews = {"username" : username, "review" : review};
+    }
     books[isbn] = book;
     return res.status(200).json({message: "review updated"});
 });
