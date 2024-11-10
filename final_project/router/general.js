55d226a7b1339bd6958c17554d3581d9bbc5e01e
bob-conroy <bob.conroymba@gmail.com>
2024-11-10 09:16:22 -0500

    Updates made

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,7 +4,6 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
-
 public_users.post("/register", (req,res) => {
   const username = req.body.username;
     const password = req.body.password;
@@ -26,20 +25,34 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  res.send(JSON.stringify(books,null,4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    res.send(books[isbn]);
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  let authBooks = []
+  if (author) {
+    for(var i in books){
+        if (books[i].author === author) {
+            authBooks.push(books[i]);
+        }
+    }    
+    if (authBooks.length > 0) {
+       res.send(JSON.stringify(authBooks,null,4));
+    } else {
+        return res.status(300).json({message: "Author not found"});
+    }
+
+  } else {
+    return res.status(300).json({message: "Author not provided"});
+  }
 });
 
 // Get all books based on title
